a22adb2e0fa97ea0b5339018eb4bbcf0f69306a3
hpome <hpome@CEREN>
2025-07-25 00:58:18 +0300

    Proje dosyası ekle.

---
+++ Days/Kaynak.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Plese enter a number (1-7) : " << endl;
+	int dayNumber;
+	cin >> dayNumber;
+
+	switch (dayNumber) {
+	case 1:
+		cout << "Monday" << endl;
+		break;
+
+	case 2:
+		cout << "Tuesday" << endl;
+		break;
+
+	case 3:
+		cout << "Wednesday" << endl;
+		break;
+
+	case 4:
+		cout << "Thursday" << endl;
+		break;
+
+	case 5:
+		cout << "Friday" << endl;
+		break;
+
+	case 6:
+		cout << "Saturday" << endl;
+		break;
+
+	case 7:
+		cout << "Sunday" << endl;
+		break;
+
+
+
+
+
+
+		
+
+	}
+}
